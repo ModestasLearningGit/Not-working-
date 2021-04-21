820e12901f5f0eda95871643dccfd409ad02b556
ModestasLearningGit <mvacerskasgit@gmail.com>
2021-04-21 21:24:07 +0300

    machine not working

---
+++ include/Definitions.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+#define SPLASH_STATE_SHOW_TIME 5.0
+
+#define SPLASH_SCREEN_BACKGROUND_FILEPATH "src/Resources/Splash_state/SPLASH_SCREEN.jpg"
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Splash_state.h"
+#include "Game.h"
+#include "Definitions.h"
+
+int main() {
+    
+    Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Flappy Bird");
+
+    return 0;
+}
